7f3aabfdea9de1d5489f736a056deee4478f9ad3
ill-soul077 <hassannaquib015@gmail.com>
2026-03-02 23:32:50 +0600

    initial commit

---
+++ src/main/java/com/minuStore/MiNu/MiNuApplication.java
@@ -0,0 +1,13 @@
+package com.minuStore.MiNu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiNuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiNuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minuStore/MiNu/MiNuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minuStore.MiNu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiNuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
