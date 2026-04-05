bb8b8fff8beeec05604b20418f7d0939991a05b6
alshariarmithu <alshariarhossain@gmail.com>
2026-04-06 00:30:22 +0600

    exception handling separate code is added

---
+++ src/main/java/com/minuStore/MiNu/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.minuStore.MiNu.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
